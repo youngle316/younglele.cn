bbd35588ebbef714548fd9474a8fb9d7e36fe13a
杨乐乐 <youngle316@gmail.com>
2022-09-14 16:49:33 +0800

    feat: change root id

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'c1bb03c15fd8476bbd30eba63ec4cec7',
+  rootNotionPageId: '4762e940fb694b999b98f3797c638c80',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
